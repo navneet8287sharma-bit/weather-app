a8567156a369d4f4c4cb4f1d045ddd6e22bf99ac
navneet8287sharma-bit <navneet8287sharma@gmail.com>
2026-06-25 12:09:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("searchBtn");
+
+button.addEventListener("click", () => {
+
+    const city = document.getElementById("city").value;
+
+    console.log(city);
+
+});
